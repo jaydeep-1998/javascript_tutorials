57001c99dde6fb1092280d6fd4be8bc71ba7d2ed
jaydeep.bhalala <jaydeep7405047637@gmail.com>
2020-07-04 12:38:26 +0530

    day1

---
+++ if_else.js
@@ -0,0 +1,25 @@
+var num = 100;
+            if(num%2==0)
+            {
+                document.write("even number\n")
+            }
+            else{
+                document.write("odd number\n")
+            }
+            if(num>0){
+                document.write("positive number\n")
+            }
+            else if(num<0){
+                document.write("negative number\n")
+            }
+            else{
+                document.write("number is 0\n")
+            }
+            if(num>0 && num%2==0){
+                document.write("positive and even number\n")
+            }
+            if(num>0){
+                if(num%2==0){
+                document.write("positive and even number\n")
+                }
+            }
---
+++ switch_case.js
@@ -0,0 +1,27 @@
+var day=50;
+//it can accepts numbers,stringd,boolean
+switch(day){
+    case 1:
+        document.write("sunday");
+        break;
+    case 2:
+        document.write("monday");
+        break;
+    case 3:
+        document.write("tuesday");
+        break;
+    case 4:
+        document.write("wednesday");
+        break;
+    case 5:
+        document.write("thursday");
+        break;
+    case 6:
+        document.write("friday");
+        break;
+    case 7:
+        document.write("saturday");
+        break;
+    default:
+        document.write("wrong input")
+}
