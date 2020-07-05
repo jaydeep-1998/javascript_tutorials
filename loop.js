d5a7281ac46a10a05a7bd38e80feacee3f85fb73
jaydeep.bhalala <jaydeep7405047637@gmail.com>
2020-07-05 08:47:30 +0530

    looping completed

---
+++ loop.js
@@ -0,0 +1,23 @@
+//for(init;condition;increment/decrement)
+for(var x=1;x<=10;x++){
+    document.write("<h2>"+5*x+"</h2>")
+}
+
+
+for(var x=1;x<=10;x+=2){
+    document.write("<h2>"+x+"</h2>")
+}
+
+
+var x=1
+while(x<10){
+    document.write("<h2>"+x+"</h2>")
+    x+=2
+}
+
+
+var x=1
+do{
+    document.write("<h2> jaydeep </h2>");
+    x++;
+}while(x<5);
